aa03a48257c383a29c11d61554af2823550f7138
SkyerAL <duyunovas@gmail.com>
2014-10-29 12:59:23 +0300

    начало проекта

---
+++ example_git/example_git/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace animal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("dsds");
+        }
+    }
+}
